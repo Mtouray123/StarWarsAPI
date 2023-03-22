7f8313d59c8f7e0ed50c9c441afcf08e3f2b0c54
Mahamadou Touray <mahamadou12@gmail.com>
2023-03-22 01:52:19 -0400

    finished

---
+++ src/Components/Species.js
@@ -0,0 +1,33 @@
+import React from "react";
+import{ Card,Grid } from 'semantic-ui-react';
+
+function Species({data}){
+    return(
+        <>
+        <h1>Star Wars Species</h1>
+        <Grid columns={3}>
+            {data.map((species, i)=>{
+                return(
+                    <Grid.Column key={i}>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>{species.name}</Card.Header>
+                                <Card.Description>
+                                    <strong>Language</strong>
+                                    <p>[{species.language}]</p>
+                                    <strong>Classification</strong>
+                                    <p>[{species.classification}]</p>
+                                    <strong>Average Lifespan</strong>
+                                    <p>[{species.average_lifespan}]</p>
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                );
+            })}
+        </Grid>
+        </>
+    );
+}
+
+export default Species;
---
+++ src/Components/Starship.js
@@ -0,0 +1,33 @@
+import React from "react";
+import{ Card,Grid } from 'semantic-ui-react';
+
+function Starships({data}){
+    return(
+        <>
+        <h1>Star Wars Starships</h1>
+        <Grid columns={3}>
+            {data.map((starships, i)=>{
+                return(
+                    <Grid.Column key={i}>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>{starships.name}</Card.Header>
+                                <Card.Description>
+                                    <strong>HyperDrive Ratings</strong>
+                                    <p>[{starships.hyperdrive_rating}]</p>
+                                    <strong>Model</strong>
+                                    <p>[{starships.model}]</p>
+                                    <strong>Manufacturer</strong>
+                                    <p>[{starships.manufacturer}]</p>
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                );
+            })}
+        </Grid>
+        </>
+    );
+}
+
+export default Starships;
